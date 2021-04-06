06fdbbe62bf9fe25d647e1d09be6606a4de39c32
Mithila Madushanka <54311243+DinethYasuru@users.noreply.github.com>
2021-04-06 09:21:05 +0530

    Start Spring

---
+++ src/main/java/com/example/Canteen_Management_Sysyem/CanteenManagementSysyemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Canteen_Management_Sysyem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenManagementSysyemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanteenManagementSysyemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Canteen_Management_Sysyem/CanteenManagementSysyemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Canteen_Management_Sysyem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenManagementSysyemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
